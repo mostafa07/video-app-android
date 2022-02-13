27bd8594d9f6991aa51063a6772d5d8d15e0695a
Mostafa Mahmoud Salem <msalem@ejada.com>
2022-02-13 21:56:35 +0200

    feat: added simple video capture use case functionality

--- app/src/main/java/com/example/android/videoapp/ui/videocapture/VideoCaptureFragment.kt
+++ app/src/main/java/com/example/android/videoapp/ui/videocapture/VideoCaptureFragment.kt
@@ -1,26 +1,243 @@
 package com.example.android.videoapp.ui.videocapture
 
+import android.Manifest
+import android.content.ContentValues
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.video.*
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.content.PermissionChecker
 import androidx.fragment.app.Fragment
+import com.example.android.videoapp.R
 import com.example.android.videoapp.databinding.FragmentVideoCaptureBinding
+import timber.log.Timber
+import java.text.SimpleDateFormat
+import java.util.*
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 
 class VideoCaptureFragment : Fragment() {
 
-    private var _binding: FragmentVideoCaptureBinding? = null
-    private val binding get() = _binding!!
+    private var _viewBinding: FragmentVideoCaptureBinding? = null
+    private val viewBinding get() = _viewBinding!!
+
+    private var videoCapture: VideoCapture<Recorder>? = null
+    private var recording: Recording? = null
+
+    // TODO use for UI
+    private lateinit var recordingState: VideoRecordEvent
+
+    private lateinit var cameraExecutor: ExecutorService
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentVideoCaptureBinding.inflate(inflater, container, false)
+        _viewBinding = FragmentVideoCaptureBinding.inflate(inflater, container, false)
 
-        binding.lifecycleOwner = viewLifecycleOwner
+        viewBinding.lifecycleOwner = viewLifecycleOwner
 
-        return binding.root
+        // Request camera permissions
+        if (allPermissionsGranted()) {
+            startCamera()
+        } else {
+            ActivityCompat.requestPermissions(
+                requireActivity(),
+                REQUIRED_PERMISSIONS,
+                REQUEST_CODE_PERMISSIONS
+            )
+        }
+
+        cameraExecutor = Executors.newSingleThreadExecutor()
+
+        setUpButtonClickListeners()
+
+
+
+        return viewBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+//        // Request camera permissions
+//        if (allPermissionsGranted()) {
+//            startCamera()
+//        } else {
+//            ActivityCompat.requestPermissions(
+//                requireActivity(),
+//                REQUIRED_PERMISSIONS,
+//                REQUEST_CODE_PERMISSIONS
+//            )
+//        }
+//
+//        cameraExecutor = Executors.newSingleThreadExecutor()
+//
+//        setUpButtonClickListeners()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+
+        _viewBinding = null
+        cameraExecutor.shutdown()
+    }
+
+
+    private fun captureVideo() {
+        val videoCapture = videoCapture ?: return
+
+        viewBinding.videoCaptureButton.isEnabled = false
+
+        val currentRecording = recording
+        if (null != currentRecording) {
+            currentRecording.stop()
+            recording = null
+            return
+        }
+
+        val name = SimpleDateFormat(FILENAME_FORMAT, Locale.US)
+            .format(System.currentTimeMillis())
+        val contentValues = ContentValues().apply {
+            put(MediaStore.MediaColumns.DISPLAY_NAME, name)
+            put(MediaStore.MediaColumns.MIME_TYPE, "video/mp4")
+            if (Build.VERSION.SDK_INT > Build.VERSION_CODES.P) {
+                put(MediaStore.Video.Media.RELATIVE_PATH, "Movies/VideoApp-Video")
+            }
+        }
+
+        val outputOptions = MediaStoreOutputOptions.Builder(
+            requireActivity().contentResolver,
+            MediaStore.Video.Media.EXTERNAL_CONTENT_URI
+        ).setContentValues(contentValues).build()
+
+        recording = videoCapture.output
+            .prepareRecording(requireActivity(), outputOptions)
+            .apply {
+                if (PermissionChecker.checkSelfPermission(
+                        requireActivity(),
+                        Manifest.permission.RECORD_AUDIO
+                    ) == PermissionChecker.PERMISSION_GRANTED
+                ) {
+                    withAudioEnabled()
+                }
+            }
+            .start(ContextCompat.getMainExecutor(requireActivity())) { recordEvent ->
+                when (recordEvent) {
+                    is VideoRecordEvent.Start -> {
+                        viewBinding.videoCaptureButton.apply {
+                            text = getString(R.string.stop_capture)
+                            isEnabled = true
+                        }
+                    }
+                    is VideoRecordEvent.Finalize -> {
+                        if (!recordEvent.hasError()) {
+                            val msg =
+                                "Video capture succeeded: " + "${recordEvent.outputResults.outputUri}"
+                            Toast.makeText(requireActivity().baseContext, msg, Toast.LENGTH_SHORT)
+                                .show()
+                            Timber.d(msg)
+                        } else {
+                            recording?.close()
+                            recording = null
+                            Timber.e("Video capture ends with error: " + recordEvent.error)
+                        }
+                        viewBinding.videoCaptureButton.apply {
+                            text = getString(R.string.start_capture)
+                            isEnabled = true
+                        }
+                    }
+                }
+            }
+    }
+
+    private fun startCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(requireActivity())
+
+        cameraProviderFuture.addListener({
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+
+            val preview = Preview.Builder()
+                .build()
+                .also {
+                    it.setSurfaceProvider(viewBinding.viewFinder.surfaceProvider)
+                }
+
+            val recorder = Recorder.Builder()
+                .setQualitySelector(
+                    QualitySelector.from(
+                        Quality.HIGHEST,
+                        FallbackStrategy.higherQualityOrLowerThan(Quality.SD)
+                    )
+                )
+                .build()
+            videoCapture = VideoCapture.withOutput(recorder)
+
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
+            try {
+                cameraProvider.unbindAll()
+                cameraProvider.bindToLifecycle(this, cameraSelector, preview, videoCapture)
+            } catch (ex: Exception) {
+                Timber.e(ex, "Use Case binding failed")
+            }
+        }, ContextCompat.getMainExecutor(requireActivity()))
+    }
+
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(
+            requireActivity().baseContext,
+            it
+        ) == PackageManager.PERMISSION_GRANTED
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            startCamera()
+        } else {
+            Toast.makeText(
+                requireContext(),
+                "Permissions not granted by ther user.",
+                Toast.LENGTH_SHORT
+            ).show()
+
+            // TODO navigate to other screen or show toast
+        }
+    }
+
+    private fun setUpButtonClickListeners() {
+        viewBinding.videoCaptureButton.setOnClickListener { captureVideo() }
+    }
+
+
+    companion object {
+        private const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
+        private const val REQUEST_CODE_PERMISSIONS = 10
+
+        private val REQUIRED_PERMISSIONS = mutableListOf(
+            Manifest.permission.CAMERA,
+            Manifest.permission.RECORD_AUDIO
+        ).apply {
+            if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.P) {
+                add(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+            }
+        }.toTypedArray()
     }
 }
